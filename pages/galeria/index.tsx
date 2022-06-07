321c2fcab76c67c0b3bdb7f35f6c03848c5a11ad
PiotrekNiemirski <piotr.niemirski@they.pl>
2022-06-07 15:58:10 +0200

    test login

--- pages/galeria/index.tsx
+++ pages/galeria/index.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import Head from "next/head";
 import { NextRouter, useRouter } from "next/router";
+import { useMediaQuery } from "react-responsive";
 import Login from "../../components/login";
+import Favicon from "../../components/favicon";
+import Cursor from "../../components/coursor/index";
+import SCREENS from "../../components/screens";
 
 interface Props {
   hasReadPermission: boolean;
 }
 
 export default function Protected({ hasReadPermission }: Props) {
-  const router: NextRouter = useRouter();
+  const isDesktop: boolean = useMediaQuery({ minWidth: SCREENS.md });
+  const [desktop, setDesktop]: [
+    boolean | undefined,
+    Dispatch<SetStateAction<boolean | undefined>>,
+  ] = useState();
+
+  useEffect(() => {
+    setDesktop(isDesktop);
+  }, []);
 
   if (!hasReadPermission) {
-    return <Login redirectPath={router.asPath} />;
+    const router: NextRouter = useRouter();
+
+    return (
+      <>
+        {desktop && <Cursor />}
+        <Head>
+          <title>Dela &amp; Piotrek – Zaloguj się</title>
+          <meta name='robots' content='noindex' />
+          <Favicon />
+        </Head>
+        <Login redirectPath={router.asPath} />
+      </>
+    );
   }
 
   return (
