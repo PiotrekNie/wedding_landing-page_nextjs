d34ad6b69dbf1660be01df479793178c78cc075d
PiotrekNiemirski <piotr.niemirski@they.pl>
2022-07-14 08:09:53 +0200

    Video live - add autoplay

--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -101,7 +101,7 @@ function Live() {
       <MainContainer>
         <IframeContainer>
           <iframe
-            src='https://www.youtube.com/embed/H1uOqct9xtA'
+            src='https://www.youtube.com/embed/H1uOqct9xtA?autoplay=1'
             title='YouTube video player'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
             allowFullScreen
