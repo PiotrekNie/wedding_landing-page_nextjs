ded0da3c418fde6bea37e2dcd552ec1e4771a51c
i:2lE;jVV}Z8tJjOAz <piotr.niemirski@gmail.com>
2023-01-08 18:29:35 +0100

    gallery - add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../src/styles/index.scss";
 import { AppProps } from "next/app";
+import Script from "next/script";
 import MouseContextProvider from "../context/mouse-context";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MouseContextProvider>
+      <Script src='https://www.googletagmanager.com/gtm.js?id=GTM-TXBWHJ8' />
       <Component {...pageProps} />
     </MouseContextProvider>
   );
---
+++ pages/_document.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import Script from "next/script";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps: DocumentInitialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang='pl'>
+        <Head>
+          <Script
+            dangerouslySetInnerHTML={{
+              __html:
+                "(function(w,l){" +
+                "w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});" +
+                "})(window,'dataLayer');",
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
